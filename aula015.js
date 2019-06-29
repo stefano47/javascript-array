c1142c5d5816b9477831af3fe190429a4bffd839
Stefano <stefanopaulo47@gmail.com>
2019-06-29 14:57:34 -0300

    concat

---
+++ aula015.js
@@ -0,0 +1,10 @@
+// Concat
+
+const filhas = ['Valeska', 'Cibalena']
+const filhos = ['Paulo', 'Pedro']
+
+const todos = filhas.concat(filhos)
+
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
